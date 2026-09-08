e3f0fa1018111554e0c0483bc88b6d8c12c9b2b3
gangesh2703 <eswarangangesh@gmail.com>
2026-09-08 09:36:12 +0530

    commit1

---
+++ placement.py
@@ -0,0 +1,51 @@
+
+print("Welcome to Python")
+
+
+print("Name: Zayan")
+print("Age: 19")
+print("College: K. Ramakrishnan College of Engineering")
+print("Department: CSE")
+
+
+name = "Zayan"
+print(name)
+print("/n")
+
+
+student_name = "Zayan"
+age = 19
+city = "Chennai"
+college = "K. Ramakrishnan College of Engineering"
+print(student_name, age, city, college)
+
+a = 10          
+b = 5.5         
+c = "Python"    
+d = True        
+print(a, b, c, d)
+
+
+age = 21
+print(type(age))
+
+
+name = "Zayan"
+age = 19
+percentage = 85.5
+is_student = True
+
+print(name, type(name))
+print(age, type(age))
+print(percentage, type(percentage))
+print(is_student, type(is_student))
+
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+age = int(input("Enter your age: "))
+print(age)
+
+percentage = float(input("Enter your percentage: "))
+print(percentage, type(percentage))
